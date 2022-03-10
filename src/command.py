5b4a9b99c45c0346cd7ed7deeed8004a3f83245b
blue_farid <farid_127@outlook.com>
2022-03-10 17:47:14 +0330

    feat: impl commands

--- src/command.py
+++ src/command.py
@@ -1,3 +1,6 @@
+from utils.string_utils import UnseenFormatter
+import data
+
 OPTIONS_STRING = '''
 min_length = {min}
 max_length = {max}
@@ -5,4 +8,41 @@ start_with = {start}
 end_with = {end}
 include = {include}
 '''
-def initial_options():
+
+formatter = UnseenFormatter()
+
+
+def options():
+    print(formatter.format(data.options_string))
+
+
+def set_min_length(min):
+    data.options_string = formatter.format(
+        data.options_string, {"min": min}
+    )
+
+
+def set_max_length(max):
+    data.options_string = formatter.format(
+        data.options_string, {"max": max}
+    )
+
+
+def set_start_with(start):
+    data.options_string = formatter.format(
+        data.options_string, {"start": start}
+    )
+
+
+def set_end_with(end):
+    data.options_string = formatter.format(
+        data.options_string, {"end": end}
+    )
+
+
+def set_include(include):
+    data.options_string = formatter.format(
+        data.options_string, {"include": include}
+    )
+
+
