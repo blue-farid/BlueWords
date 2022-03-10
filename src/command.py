5fc0267da5893e333db0f819eeaf4655cb536503
blue_farid <farid_127@outlook.com>
2022-03-10 18:51:06 +0330

    feat: rebuild commands and delete unuse files

--- src/command.py
+++ src/command.py
@@ -1,48 +1,63 @@
-from utils.string_utils import UnseenFormatter
-import data
-
-OPTIONS_STRING = '''
-min_length = {min}
-max_length = {max}
-start_with = {start}
-end_with = {end}
-include = {include}
-'''
-
-formatter = UnseenFormatter()
+from enum import Enum
 
 
-def options():
-    print(formatter.format(data.options_string))
+class Attribute(Enum):
+    MIN = "min_length"
+    MAX = "max_length"
+    START = "start_with"
+    END = "end_with"
+    INCLUDE = "include"
+    HAS_DIGIT = "has_digit"
+    HAS_SPECIAL = "has_special_character"
+    HAS_LOWER = "has_lower_case_character"
+    HAS_UPPER = "has_upper_case_character"
+
+
+options_dict = {Attribute.MIN: 5,
+                Attribute.MAX: 5,
+                Attribute.START: '?',
+                Attribute.END: '?',
+                Attribute.INCLUDE: '?'}
+
+
+def print_options():
+    for option, value in options_dict:
+        print(f"{option} = {value}")
 
 
 def set_min_length(min):
-    data.options_string = formatter.format(
-        data.options_string, {"min": min}
-    )
+    options_dict[Attribute.MIN] = min
 
 
 def set_max_length(max):
-    data.options_string = formatter.format(
-        data.options_string, {"max": max}
-    )
+    options_dict[Attribute.MAX] = max
 
 
 def set_start_with(start):
-    data.options_string = formatter.format(
-        data.options_string, {"start": start}
-    )
+    options_dict[Attribute.START] = start
 
 
 def set_end_with(end):
-    data.options_string = formatter.format(
-        data.options_string, {"end": end}
-    )
+    options_dict[Attribute.END] = end
 
 
 def set_include(include):
-    data.options_string = formatter.format(
-        data.options_string, {"include": include}
-    )
+    options_dict[Attribute.INCLUDE] = include
+
+
+def set_has_digit(has_digit):
+    options_dict[Attribute.HAS_DIGIT] = has_digit
+
+
+def set_has_special(has_special):
+    options_dict[Attribute.HAS_SPECIAL] = has_special
+
+
+def set_has_lower(has_lower):
+    options_dict[Attribute.HAS_LOWER] = has_lower
+
+
+def set_has_upper(has_upper):
+    options_dict[Attribute.HAS_UPPER] = has_upper
 
 
--- src/data.py
+++
@@ -1,7 +0,0 @@
-options_string = '''
-min_length = {min}
-max_length = {max}
-start_with = {start}
-end_with = {end}
-include = {include}
-'''
