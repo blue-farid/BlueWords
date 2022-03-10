70919a41eccc97eb0255e421ae59caa89b8eb6e9
blue_farid <farid_127@outlook.com>
2022-03-10 23:20:16 +0330

    feat: add other commands in main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import command
-
+from command import Attribute
 
 def process(input):
     input = input.strip()
@@ -13,30 +13,47 @@ def process(input):
 
     inputs = input.split(" ")
     if len(inputs) == 1:
-        if inputs[0] == "options":
-            command.options()
+        if inputs[0] == Attribute.OPTIONS:
+            command.print_options()
         elif inputs[0] == "exit":
             return 1
         else:
             return -1
-    if len(inputs) == 2:
+    elif len(inputs) == 2:
         if inputs[0] == "set":
-            if inputs[1] == "start_with":
+            if inputs[1] == Attribute.START:
                 command.set_start_with(phrase)
-            elif inputs[1] == "end_with":
+            elif inputs[1] == Attribute.END:
                 command.set_end_with(phrase)
-            elif inputs[1] == "include":
+            elif inputs[1] == Attribute.INCLUDE:
                 command.set_include(phrase)
             else:
                 return -1
-    if len(inputs == 3):
-        if inputs[0] == "set" and inputs[2].isdigit():
-            if inputs[1] == "max_length":
-                command.set_max_length(inputs[2])
-            elif inputs[1] == "min_length":
-                command.set_min_length(inputs[2])
+    elif len(inputs == 3):
+        if inputs[0] == "set":
+            if inputs[2].isdigit():
+                if inputs[1] == Attribute.MAX:
+                    command.set_max_length(inputs[2])
+                elif inputs[1] == Attribute.MIN:
+                    command.set_min_length(inputs[2])
+                else:
+                    return -1
+            elif inputs[2] in ["True", "False"]:
+                res = inputs[2] == "True"
+                if inputs[1] == Attribute.HAS_UPPER:
+                    command.set_has_upper(res)
+                elif inputs[1] == Attribute.HAS_LOWER:
+                    command.set_has_lower(res)
+                elif inputs[1] == Attribute.HAS_SPECIAL:
+                    command.set_has_special(res)
+                elif inputs[1] == Attribute.HAS_DIGIT:
+                    command.set_has_digit(res)
+                else:
+                    return -1
             else:
                 return -1
+    else:
+        return -1
     return 0
 
 
